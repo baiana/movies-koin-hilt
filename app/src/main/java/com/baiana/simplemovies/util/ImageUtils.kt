52ed23616b4047d6a2d10859602db8425c2d00a0
Ana Dias <analuisa104@gmail.com>
2020-10-13 12:01:32 -0300

    feat: create image utils

---
+++ app/src/main/java/com/baiana/simplemovies/util/ImageUtils.kt
@@ -0,0 +1,15 @@
+package com.baiana.simplemovies.util
+
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.request.RequestOptions
+
+fun ImageView.loadWithGlide(url: String) {
+    val options: RequestOptions = RequestOptions().transform(CenterCrop())
+
+    Glide.with(this)
+        .load(url).thumbnail(0.3f)
+        .apply(options)
+        .into(this)
+}
